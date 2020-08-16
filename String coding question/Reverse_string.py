d93abbab0f3dee093eba98404b568294dedf8a19
Ajay Jaitwal <51505382+AJAYJAITWAL@users.noreply.github.com>
2020-08-16 17:42:06 +0530

    Update Reverse_string.py

--- String coding question/Reverse_string.py
+++ String coding question/Reverse_string.py
@@ -1,4 +1,21 @@
 # write a program to reverse given string.
+
+#This is a first method
 s=input("Enter the string")
 output = s[::-1]
+print(output)
+
+#This is a second method
+s=input("Enter the string")
+r=reversed(s)
+output=''.join(r)
+print(output)
+
+#This is a third method
+s=input("Enter any string")
+output=''
+i = len(s)-1
+while i>=0:
+	output+=s[i]
+	i=i-1
 print(output)
